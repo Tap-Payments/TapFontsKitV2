39fcab109f506ace6773fe2f86fbf24f14316f08
O Rabie <o.rabie@tap.company>
2020-09-22 13:24:04 +0200

    Upgrade to Xcode12+ compiler friendly code

---
+++ TapFontsKitV2/TapFontsKitV2/TapFontsKitV2.h
@@ -0,0 +1,18 @@
+//
+//  TapFontsKitV2.h
+//  TapFontsKitV2
+//
+//  Created by Osama Rabie on 9/22/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapFontsKitV2.
+FOUNDATION_EXPORT double TapFontsKitV2VersionNumber;
+
+//! Project version string for TapFontsKitV2.
+FOUNDATION_EXPORT const unsigned char TapFontsKitV2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapFontsKitV2/PublicHeader.h>
+
+
